caeb868a0a82fabb5b52a009fbccb23c4e5ce618
rubenberna <rubenmbernardes@gmail.com>
2025-07-07 11:25:28 +0200

    Ref: only call history and loading messages when needed

--- src/hooks/_useOnMount.ts
+++ src/hooks/_useOnMount.ts
@@ -2,7 +2,7 @@ import useSWR from "swr";
 import {fetcher} from "@/src/lib/utils/functions";
 
 export const _useOnMount = () => {
-  const {data: count, isLoading: isLoadingCount, isValidating} = useSWR('/api/getCount', fetcher)
+  const {data: count, isLoading: isLoadingCount} = useSWR('/api/getCount', fetcher)
   const {isLoading} = useSWR(!isLoadingCount && count < 1 ? '/api/loadPdf' : null, fetcher)
   return {
     isLoadingPdf: isLoading,
